bf6dd801aa92657d78a889bdd3218342be8358f4
mohankalimuthu <mohankalimuthu2004@gmail.com>
2025-12-28 18:35:58 +0530

    database

--- database.py
+++ database.py
@@ -3,11 +3,6 @@ class Database:
         self.login_storage = [0]*200
         self.register_storage = {}
 
-#    def input_transfer(self,fname,lname,email,password):
-#        db_transfer_val = reg.hashing_transfer(email,password)
-#        self.login_storage[db_transfer_val[0]] = db_transfer_val[1]
-#        self.register_storage.update({db_transfer_val[0]:[fname,lname,email,db_transfer_val[0]]})
-
     def login_store(self,res):
         self.login_storage[res[0]] = res[1]
         print(self.login_storage)
@@ -18,7 +13,5 @@ class Database:
 
 
 db_ = Database()
-#db_.input_transfer("mohan","kalimuthu","mohanmohan89057@gmail.com","8601")
-#db_.input_transfer("abinaya","subramani","abisubramani8601@gmail.com","10688601")
 
 
